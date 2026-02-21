55dcf715c7304c3b1924da67e63c829e7d30e1ac
shyam <shyam9.r9@gmail.com>
2026-02-21 12:30:51 +0530

    6. Conditions

---
+++ 6.Conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NUMBER=999
+if [ $1 -gt $NUMBER ]; then
+    echo"$1 is greater than $NUMBER"
+elif[ $1 -lt $NUMBER ]; then
+    echo "$1 is smaller than $NUMBER"
+else
+    echo "Ented number $1 is same as comparision number $NUMBER"
+
+fi
+
+
