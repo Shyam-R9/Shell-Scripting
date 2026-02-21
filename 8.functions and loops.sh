ce5d973fbd4cc2dfc8da5b6af14d5b28d7b17240
shyam <shyam9.r9@gmail.com>
2026-02-21 18:58:03 +0530

    8.functions

---
+++ 8.functions and loops.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#install 3 packages - mysql, nginx, python3
+
+#check if the user has privilages to install the softwares
+if [ $(id -u) -ne 0 ]; then
+    echo "You do not have sudo privilages to install the softwares"
+    exit 1
+else
+    #checking if software is already installed"
+    if ! rpm -q mysql &>/dev/null; then
+        echo "mysql not found. Proceeding to install"
+        #installing the software
+        if dnf install mysql -y ; then
+            echo "mysql installed successfully"
+            exit 0
+        else
+            echo "mysql installation failed"
+            exit 1
+        fi
+    else
+        echo "mysql already installed on this system"
+        exit 0
+    fi
+
+    #checking if software is already installed"
+    if ! rpm -q nginx &>/dev/null; then
+        echo "nginx not found. Proceeding to install"
+        #installing the software
+        if dnf install nginx -y ; then
+            echo "nginx installed successfully"
+            exit 0
+        else
+            echo "nginx installation failed"
+            exit 1
+        fi
+    else
+        echo "nginx already installed on this system"
+        exit 0
+    fi
+
+    #checking if software is already installed"
+    if ! rpm -q python3 &>/dev/null; then
+        echo "python not found. Proceeding to install"
+        #installing the software
+        if dnf install python3 -y ; then
+            echo "python installed successfully"
+            exit 0
+        else
+            echo "python installation failed"
+            exit 1
+        fi
+    else
+        echo "python already installed on this system"
+        exit 0
+    fi
+
+
+fi
+
+
