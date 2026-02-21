12fd842139af1895344330ecd0dbda00b6882e28
shyam <shyam9.r9@gmail.com>
2026-02-21 18:46:52 +0530

    8.functions

---
+++ 8.functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#install 3 packages - mysql, nginx, python3
+
+#check if the user has privilages to install the softwares
+if [ $(id -u) -ne 0 ]; then
+    echo "You do not have sudo privilages to install the softwares"
+    exit 1
+else
+    #checking if software is already installed"
+    if ! rpm -q mysql &>/dev/null; then
+        echo "mysql not found. Proceeding to install"
+        #installing the software
+        if dnf install mysql -y ; then
+            echo "mysql installed successfully"
+            exit 0
+        else
+            echo "mysql installation failed"
+            exit 1
+    else
+        echo "my sql already installed on this system"
+        exit 0
+    fi
+fi
+
+
