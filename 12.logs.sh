130506bacfb796dd2f7671d6403114eb35248d51
shyam <shyam9.r9@gmail.com>
2026-02-21 22:27:59 +0530

    12.logs

---
+++ 12.logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#install packages
+# colour code
+
+R="\e[31m" G="\e[32m" Y="\e[33m" N="\e[0m"
+
+#Check if the user has privileges to install the packages
+if [ $( id - u) -ne 0 ]; then
+    echo -e "$R You don't have privileges to install the packages $N"
+fi
+
+install_pkg() {
+    local pkg=$1
+    if ! rpm -q $pkg &>/dev/null; then
+        echo -e "$Y $pkg is not found on this system. Proceeding to install $N"
+        if dnf install "$pkg" -y ; then
+            echo -e "$G $pkg installed successfully $N"
+        else
+            echo-e "$R $pkg failed to install $N"
+        fi
+    else
+        echo -e "$Y $pkg already available on this system $N"
+    fi
+}
+packages=("mysql" "nginx" "python3")
+
+for "package" in "${packages[@]}"; do
+    install_pkg "$package"
+done
+
+echo -e "$R All checks completed"
+exit 0
